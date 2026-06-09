d2f58c81033346bce4c5cd7f53a5b54f5bef593c
Michael Naumov <mnaoumov@gmail.com>
2026-06-08 21:51:47 -0600

    chore: update libs

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 [![Buy Me a Coffee](https://img.shields.io/badge/Buy%20Me%20a%20Coffee-ffdd00?logo=buy-me-a-coffee&logoColor=black)](https://www.buymeacoffee.com/mnaoumov)
 [![GitHub release](https://img.shields.io/github/v/release/mnaoumov/obsidian-new-note-fixer)](https://github.com/mnaoumov/obsidian-new-note-fixer/releases)
 [![GitHub downloads](https://img.shields.io/github/downloads/mnaoumov/obsidian-new-note-fixer/total)](https://github.com/mnaoumov/obsidian-new-note-fixer/releases)
+[![Coverage: 100%](https://img.shields.io/badge/coverage-100%25-brightgreen)](https://github.com/mnaoumov/obsidian-new-note-fixer)
 
 This is a plugin for [Obsidian](https://obsidian.md/) that unifies the way non-existing notes are created when clicking on their links.
 
--- scripts/vitest-config.ts
+++ scripts/vitest-config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vitest/config';
 
+const SHARED_EXCLUDE = ['node_modules', 'dist'];
+const BIG_TIMEOUT_IN_MILLISECONDS = 30_000;
+const ANDROID_TIMEOUT_IN_MILLISECONDS = 60_000;
+const HOOK_TIMEOUT_MULTIPLIER = 4;
+
 export const config = defineConfig({
   test: {
     coverage: {
@@ -14,17 +19,68 @@ export const config = defineConfig({
     exclude: ['node_modules', 'dist'],
     globals: false,
     include: ['src/**/*.test.ts'],
+    passWithNoTests: true,
     projects: [
       {
+        resolve: {
+          alias: {
+            obsidian: 'obsidian-test-mocks/obsidian'
+          }
+        },
         test: {
-          environment: 'node',
+          environment: 'jsdom',
+          exclude: [...SHARED_EXCLUDE, 'src/**/*.integration.test.ts'],
+          execArgv: ['--no-webstorage'],
           include: ['src/**/*.test.ts'],
           name: 'unit-tests',
           server: {
             deps: {
               inline: ['@obsidian-typings', 'obsidian-dev-utils']
             }
-          }
+          },
+          setupFiles: [
+            'obsidian-test-mocks/vitest-setup',
+            'obsidian-test-mocks/obsidian-typings/vitest-setup'
+          ]
+        }
+      },
+      {
+        test: {
+          environment: 'node',
+          fileParallelism: false,
+          hookTimeout: BIG_TIMEOUT_IN_MILLISECONDS * HOOK_TIMEOUT_MULTIPLIER,
+          include: ['src/**/*.no-app.integration.test.ts'],
+          name: 'integration-tests:no-app',
+          testTimeout: BIG_TIMEOUT_IN_MILLISECONDS
+        }
+      },
+      {
+        test: {
+          environment: 'node',
+          fileParallelism: false,
+          globalSetup: ['obsidian-integration-testing/vitest-global-setup'],
+          hookTimeout: BIG_TIMEOUT_IN_MILLISECONDS * HOOK_TIMEOUT_MULTIPLIER,
+          include: ['src/**/*.desktop.integration.test.ts'],
+          name: 'integration-tests:desktop',
+          testTimeout: BIG_TIMEOUT_IN_MILLISECONDS
+        }
+      },
+      {
+        test: {
+          environment: 'node',
+          environmentOptions: {
+            obsidianTransport: {
+              appiumUrl: 'http://localhost:4723',
+              avdName: 'obsidian_test',
+              type: 'obsidian-android-appium'
+            }
+          },
+          fileParallelism: false,
+          globalSetup: ['obsidian-integration-testing/vitest-global-setup'],
+          hookTimeout: ANDROID_TIMEOUT_IN_MILLISECONDS * HOOK_TIMEOUT_MULTIPLIER,
+          include: ['src/**/*.android.integration.test.ts'],
+          name: 'integration-tests:android',
+          testTimeout: ANDROID_TIMEOUT_IN_MILLISECONDS
         }
       }
     ]
